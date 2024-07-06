98618594dbbf34b8ed2a962c30f4b2182c6456cb
BroSible <mikhail.petr2005@gmail.com>
2024-07-06 23:07:44 +0300

    update playerController
    Add a gravity for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 6.0f;
+     public float speed = 6.0f;
+    public float gravity = 20.0f;
     private CharacterController controller;
     private Vector3 moveDirection = Vector3.zero;
 
@@ -13,13 +14,17 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        if (controller.isGrounded)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
 
-        moveDirection = new Vector3(moveHorizontal, 0, moveVertical);
-        moveDirection = Vector3.ClampMagnitude(moveDirection, 1);
-        moveDirection *= speed;
+            moveDirection = new Vector3(moveHorizontal, 0, moveVertical);
+            moveDirection = Vector3.ClampMagnitude(moveDirection, 1);
+            moveDirection *= speed;
+        }
 
+        moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
     }
 }
